84c6ae0ea0bcbd8418f45e78ac181fb2af95687e
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-09-03 17:06:12 +0530

    Add files via upload

---
+++ Day-02 (03-09-2024)/palindrome.py
@@ -0,0 +1,12 @@
+# Print a triangle with palindrome numbers
+
+spaces = 40
+for i in range(1,10):
+    for j in range(0,spaces):
+        print(" ",end="")
+    for j in range(1, i):
+        print(j, end="")
+    for j in range(i, 0, -1):
+        print(j, end="")
+    print()
+    spaces = spaces - 1
