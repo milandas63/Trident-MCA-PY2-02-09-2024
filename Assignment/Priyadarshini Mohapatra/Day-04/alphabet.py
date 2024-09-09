f16a27a2a39c56df17b3fd7029e36cd5cb046818
Priyadarshini Mohapatra <priyadarshinimohapatra328@gmail.com>
2024-09-09 16:03:39 +0530

    Update alphabet.py

--- Assignment/Priyadarshini Mohapatra/Day-04/alphabet.py
+++ Assignment/Priyadarshini Mohapatra/Day-04/alphabet.py
@@ -8,3 +8,21 @@ for i in range (0,11):
         print(chr(96+j),end=" ")
     print()
     space-=1
+
+
+
+OR
+
+
+#How many times a particular character has occured
+data="Student Allowed"
+count=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for each in data:
+    asc = ord(each)
+    if asc>=65 and asc<=90:
+        count[asc-65] = count[asc-65] + 1
+    elif asc>=97 and asc<=122:
+        count [asc-97] = count[asc-97] +1
+for i in range (0,len(count)):
+    if count[i]>0:
+        print(chr(i+65), "=", count[i])
