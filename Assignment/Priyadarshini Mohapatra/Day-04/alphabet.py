e1d4c89769244738e5eef82705400bf774bc0b6c
Priyadarshini Mohapatra <priyadarshinimohapatra328@gmail.com>
2024-09-09 15:46:11 +0530

    Update alphabet.py

--- Assignment/Priyadarshini Mohapatra/Day-04/alphabet.py
+++ Assignment/Priyadarshini Mohapatra/Day-04/alphabet.py
@@ -1,7 +1,7 @@
 space=30
 for i in range (0,11):
     for j in range(0,space):
-        print(" ",end=" ")
+        print(" ",end="")
     for j in range (1,i):
         print(chr(96+j),end=" ")
     for j in range (i,0,-1):
