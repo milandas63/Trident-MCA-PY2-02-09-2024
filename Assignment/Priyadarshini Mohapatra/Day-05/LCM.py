72a9a1230d47ca0a3578bb130fbd073850f6d671
Priyadarshini Mohapatra <priyadarshinimohapatra328@gmail.com>
2024-09-10 09:12:06 +0530

    Add files via upload

---
+++ Assignment/Priyadarshini Mohapatra/Day-05/LCM.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter a number1:"))
+num2=int(input("Enter a number2:"))
+for i in range(max(num1, num2), 1 + (num1 * num2)):
+    if i % num1 == i % num2 == 0:
+        lcm = i
+        break
+print("LCM of", num1, "and", num2, "is", lcm)
---
+++ Assignment/Priyadarshini Mohapatra/Day-05/sqroot.py
@@ -0,0 +1,3 @@
+num=float(input("Enter a number:"))
+import math
+print(int(math.sqrt(num)))
