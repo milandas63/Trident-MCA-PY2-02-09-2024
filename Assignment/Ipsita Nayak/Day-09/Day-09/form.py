1841fb4c4fba30ef4eb327c7a7c4ea14c2f0769b
Ipsita Nayak <ipsitanayak923@gmail.com>
2024-09-18 13:39:20 +0530

    Add files via upload

---
+++ Assignment/Ipsita Nayak/Day-09/Day-09/form.py
@@ -0,0 +1,13 @@
+name = input("Your name: ")
+age = input("Your age: ")
+dob = input("Date of birth (dd-mm-yyyy): ")
+gender = input("Gender [M/F]: ")
+mob = input("Mobile No.: ")
+email = input("Email-id: ")
+
+print("Name:".ljust(20), name)
+print("Age:".ljust(20), age)
+print("DOB:".ljust(20), dob)
+print("Gender (M/F):".ljust(20), gender)
+print("Mobile No.:".ljust(20), mob)
+print("Email-id:".ljust(20), email)
