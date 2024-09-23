0d891b36acc813d2ddbdf1ee8c1924c98973378b
Priyadarshini Mohapatra <priyadarshinimohapatra328@gmail.com>
2024-09-23 07:02:07 +0530

    Add files via upload

---
+++ Assignment/Priyadarshini Mohapatra/Day-12/first char.py
@@ -0,0 +1,11 @@
+def intial(name):
+    data=name.split()
+    buf=""
+    for name in data:
+        buf= buf + name[0]
+    return buf
+print(intial("Priyadarshini Mohapatra"))
+
+    
+    
+    
