d941c357c97c4e6ff8eced94e570ee442b9bd8dd
Srikant Behera <kumarsrikant0788@gmail.com>
2024-11-20 12:38:14 +0530

    Add files via upload

---
+++ Assignment/Srikant Behera/DAY-31/contact_info.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = '2003',
+	database = 'info_db'
+)
+
+curs = conn.cursor()
+curs.execute("SELECT s.rool_no AS 'Roll_no', s.student_name AS 'Student_name', c.class_desc AS 'Class_desc', s.section_id AS 'Section_id' , i.inoculation_desc AS 'Inoculation_id' FROM student AS s LEFT JOIN s.class_desc=c.class_desc LEFT JOIN s.inoculation=i.inoculation_desc;")
+
+for row in curs:
+	for col in row:
+		print(col, end='  ')
+	print()
