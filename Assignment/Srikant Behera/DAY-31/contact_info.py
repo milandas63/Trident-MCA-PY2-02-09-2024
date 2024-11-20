bfdb45c8f302747b9160254d443fbfb1465f369f
Srikant Behera <kumarsrikant0788@gmail.com>
2024-11-20 13:04:59 +0530

    Update contact_info.py

--- Assignment/Srikant Behera/DAY-31/contact_info.py
+++ Assignment/Srikant Behera/DAY-31/contact_info.py
@@ -8,7 +8,7 @@ conn = mysql.connector.connect(
 )
 
 curs = conn.cursor()
-curs.execute("SELECT s.rool_no AS 'Roll_no', s.student_name AS 'Student_name', c.class_desc AS 'Class_desc', s.section_id AS 'Section_id' , i.inoculation_desc AS 'Inoculation_id' FROM student AS s LEFT JOIN s.class_desc=c.class_desc LEFT JOIN s.inoculation=i.inoculation_desc;")
+curs.execute("SELECT s.rool_no AS 'Roll_no', s.student_name AS 'Student_name', c.class_desc AS 'Class_desc', s.section_id AS 'Section_id' , i.inoculation_desc AS 'Inoculation_id' FROM student AS s LEFT JOIN class AS c ON s.class_desc=c.class_desc LEFT JOIN inoculation AS i ON s.inoculation=i.inoculation_desc;")
 
 for row in curs:
 	for col in row:
